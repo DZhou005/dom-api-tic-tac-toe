f71504049d638112bd7b37e8a6c001984a284239
Savannah Rice <savannah.rice@gmail.com>
2021-01-07 16:35:24 -0700

    Added images

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,48 @@
-const currentPlayerSymbol = "x"
-const squareValues = ["" ,"", "", "", "", "","",""]
+// const currentPlayerSymbol = "x"
+// const squareValues = ["" ,"", "", "", "", "","",""]
 
 window.addEventListener("DOMContentLoaded", event => {
-const grid = document.body.getElementById("tic-tac-toe-board");
+const currentPlayerSymbol = "o";
+const squareValues = ["" ,"", "", "", "", "","",""];
+const grid = document.getElementById("tic-tac-toe-board");
+const xImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg';
+const oImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg';
+
 grid.addEventListener("click", (event) => {
   const clickItem = event.target.id;
   if (clickItem.includes("square-")) {
+    const boardNumber = Number.parseInt(clickItem[clickItem.length-1]);
+
+    if(squareValues[boardNumber] === ''){
+      squareValues[boardNumber] = boardNumber;
+      const icon = document.createElement('img');
+      
+      if (currentPlayerSymbol === 'x'){
+        icon.src = xImg;
+
+        event.target.appendChild(icon);
+      }
+      else {
+        icon.src = oImg;
+
+        event.target.appendChild(icon);
+
+      }
+
+
+      
+
+    }
+
+
+
     
   }
 
 })
 
-
+// const square = document.getElementById('square-0');
+// console.log(Number.parseInt(square.id[square.id.length-1]));
 
 
 
