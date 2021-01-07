4d3f4156d5b1f67c2cf24f69492f24be332368eb
Danny Zhou <dzhou005@ucr.edu>
2021-01-07 14:29:46 -0800

    first

---
+++ tic-tac-toe.js
@@ -0,0 +1,23 @@
+const currentPlayerSymbol = "x"
+const squareValues = ["" ,"", "", "", "", "","",""]
+
+window.addEventListener("DOMContentLoaded", event => {
+const grid = document.body.getElementById("tic-tac-toe-board");
+grid.addEventListener("click", (event) => {
+  const clickItem = event.target.id;
+  if (clickItem.includes("square-")) {
+    
+  }
+
+})
+
+
+
+
+
+
+
+
+
+
+})
