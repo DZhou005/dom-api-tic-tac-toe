3dde7124d362b8337056daceeea13bd4faf0b22c
Danny Zhou <dzhou005@ucr.edu>
2021-01-07 16:27:06 -0800

    finished setting winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,48 +1,78 @@
-// const currentPlayerSymbol = "x"
+let currentPlayerSymbol = "x"
 // const squareValues = ["" ,"", "", "", "", "","",""]
+const gameStatus = ""
 
+function checkGameStatus(arr) {
+  if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] !== "") {
+    console.log("yay you won!")
+    return
+  }
+  else if (arr[3] === arr[4] && arr[3] === arr[5] && arr[3] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[6] === arr[7] && arr[6] === arr[8] && arr[6] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[0] === arr[3] && arr[0] === arr[6] && arr[0] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[1] === arr[4] && arr[1] === arr[7] && arr[1] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[2] === arr[5] && arr[2] === arr[8] && arr[2] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== "") {
+    console.log("yay you won!")
+    return
+  } else if (arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== "") {
+    console.log("yay you won!")
+    return
+  }
+
+}
 window.addEventListener("DOMContentLoaded", event => {
-const currentPlayerSymbol = "o";
-const squareValues = ["" ,"", "", "", "", "","",""];
-const grid = document.getElementById("tic-tac-toe-board");
-const xImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg';
-const oImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg';
+  // let currentPlayerSymbol = "o";
+  const squareValues = ["", "", "", "", "", "", "", "", ""];
+  const grid = document.getElementById("tic-tac-toe-board");
+  const xImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg';
+  const oImg = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg';
 
-grid.addEventListener("click", (event) => {
-  const clickItem = event.target.id;
-  if (clickItem.includes("square-")) {
-    const boardNumber = Number.parseInt(clickItem[clickItem.length-1]);
+  grid.addEventListener("click", (event) => {
+    const clickItem = event.target.id;
+    if (clickItem.includes("square-")) {
+      const boardNumber = Number.parseInt(clickItem[clickItem.length - 1]);
 
-    if(squareValues[boardNumber] === ''){
-      squareValues[boardNumber] = boardNumber;
-      const icon = document.createElement('img');
-      
-      if (currentPlayerSymbol === 'x'){
-        icon.src = xImg;
+      if (squareValues[boardNumber] === '') {
+        squareValues[boardNumber] = currentPlayerSymbol;
+        const icon = document.createElement('img');
+        if (currentPlayerSymbol === 'x') {
+          icon.src = xImg;
 
-        event.target.appendChild(icon);
+          event.target.appendChild(icon);
+          currentPlayerSymbol = "o"
+        }
+        else {
+          icon.src = oImg;
+
+          event.target.appendChild(icon);
+          currentPlayerSymbol = "x"
+        }
+
+        checkGameStatus(squareValues)
+        
+
       }
-      else {
-        icon.src = oImg;
-
-        event.target.appendChild(icon);
-
-      }
 
 
-      
+
 
     }
 
+  })
 
-
-    
-  }
-
-})
-
-// const square = document.getElementById('square-0');
-// console.log(Number.parseInt(square.id[square.id.length-1]));
+  // const square = document.getElementById('square-0');
+  // console.log(Number.parseInt(square.id[square.id.length-1]));
 
 
 
